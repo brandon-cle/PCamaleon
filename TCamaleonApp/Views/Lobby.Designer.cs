f919e2a91dfa1c6ecbdb16c3aad137dcabe83603
brandon-cle <leninbrandon805@gmail.com>
2021-11-29 23:19:44 -0600

    Final stage of decoration frm

--- TCamaleonApp/Views/Lobby.Designer.cs
+++ TCamaleonApp/Views/Lobby.Designer.cs
@@ -34,13 +34,10 @@
             this.btn_employees = new System.Windows.Forms.Button();
             this.btn_Service = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.btn_close = new System.Windows.Forms.PictureBox();
-            this.btn_minimize = new System.Windows.Forms.PictureBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
             this.dl_roluser = new System.Windows.Forms.Label();
             this.dl_username = new System.Windows.Forms.Label();
@@ -49,14 +46,17 @@
             this.panel4 = new System.Windows.Forms.Panel();
             this.dl_disconnected = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.pb_typeofuser = new System.Windows.Forms.PictureBox();
+            this.btn_close = new System.Windows.Forms.PictureBox();
+            this.btn_minimize = new System.Windows.Forms.PictureBox();
             this.ContentPanel.SuspendLayout();
             this.panel2.SuspendLayout();
             this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.btn_close)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.btn_minimize)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.panel3.SuspendLayout();
             this.panel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_typeofuser)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btn_close)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btn_minimize)).BeginInit();
             this.SuspendLayout();
             // 
             // ContentPanel
@@ -68,7 +68,7 @@
             this.ContentPanel.Dock = System.Windows.Forms.DockStyle.Fill;
             this.ContentPanel.Location = new System.Drawing.Point(0, 0);
             this.ContentPanel.Name = "ContentPanel";
-            this.ContentPanel.Size = new System.Drawing.Size(1155, 692);
+            this.ContentPanel.Size = new System.Drawing.Size(1032, 688);
             this.ContentPanel.TabIndex = 0;
             // 
             // panel_painting
@@ -77,7 +77,7 @@
             this.panel_painting.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel_painting.Location = new System.Drawing.Point(265, 34);
             this.panel_painting.Name = "panel_painting";
-            this.panel_painting.Size = new System.Drawing.Size(890, 658);
+            this.panel_painting.Size = new System.Drawing.Size(767, 654);
             this.panel_painting.TabIndex = 2;
             this.panel_painting.Paint += new System.Windows.Forms.PaintEventHandler(this.panel_painting_Paint);
             // 
@@ -95,7 +95,7 @@
             this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel2.Location = new System.Drawing.Point(0, 34);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(265, 658);
+            this.panel2.Size = new System.Drawing.Size(265, 654);
             this.panel2.TabIndex = 1;
             // 
             // btn_employees
@@ -107,7 +107,7 @@
             this.btn_employees.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(237)))), ((int)(((byte)(239)))));
             this.btn_employees.Location = new System.Drawing.Point(0, 438);
             this.btn_employees.Name = "btn_employees";
-            this.btn_employees.Size = new System.Drawing.Size(265, 58);
+            this.btn_employees.Size = new System.Drawing.Size(265, 54);
             this.btn_employees.TabIndex = 1;
             this.btn_employees.Text = "Empleados";
             this.btn_employees.UseVisualStyleBackColor = false;
@@ -122,7 +122,7 @@
             this.btn_Service.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(237)))), ((int)(((byte)(239)))));
             this.btn_Service.Location = new System.Drawing.Point(0, 246);
             this.btn_Service.Name = "btn_Service";
-            this.btn_Service.Size = new System.Drawing.Size(265, 58);
+            this.btn_Service.Size = new System.Drawing.Size(265, 54);
             this.btn_Service.TabIndex = 0;
             this.btn_Service.Text = "Servicio";
             this.btn_Service.UseVisualStyleBackColor = false;
@@ -136,32 +136,8 @@
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1155, 34);
+            this.panel1.Size = new System.Drawing.Size(1032, 34);
             this.panel1.TabIndex = 0;
-            // 
-            // btn_close
-            // 
-            this.btn_close.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btn_close.Image = global::TCamaleonApp.Properties.Resources.close;
-            this.btn_close.Location = new System.Drawing.Point(1122, 3);
-            this.btn_close.Name = "btn_close";
-            this.btn_close.Size = new System.Drawing.Size(30, 29);
-            this.btn_close.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.btn_close.TabIndex = 0;
-            this.btn_close.TabStop = false;
-            this.btn_close.Click += new System.EventHandler(this.btn_close_Click);
-            // 
-            // btn_minimize
-            // 
-            this.btn_minimize.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btn_minimize.Image = global::TCamaleonApp.Properties.Resources.minus;
-            this.btn_minimize.Location = new System.Drawing.Point(1087, 4);
-            this.btn_minimize.Name = "btn_minimize";
-            this.btn_minimize.Size = new System.Drawing.Size(29, 28);
-            this.btn_minimize.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.btn_minimize.TabIndex = 1;
-            this.btn_minimize.TabStop = false;
-            this.btn_minimize.Click += new System.EventHandler(this.btn_minimize_Click);
             // 
             // button1
             // 
@@ -172,7 +148,7 @@
             this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(237)))), ((int)(((byte)(239)))));
             this.button1.Location = new System.Drawing.Point(0, 310);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(265, 58);
+            this.button1.Size = new System.Drawing.Size(265, 54);
             this.button1.TabIndex = 2;
             this.button1.Text = "Cliente";
             this.button1.UseVisualStyleBackColor = false;
@@ -186,7 +162,7 @@
             this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(237)))), ((int)(((byte)(239)))));
             this.button2.Location = new System.Drawing.Point(0, 374);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(265, 58);
+            this.button2.Size = new System.Drawing.Size(265, 54);
             this.button2.TabIndex = 3;
             this.button2.Text = "Usuario";
             this.button2.UseVisualStyleBackColor = false;
@@ -200,7 +176,7 @@
             this.button3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(237)))), ((int)(((byte)(239)))));
             this.button3.Location = new System.Drawing.Point(0, 491);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(265, 58);
+            this.button3.Size = new System.Drawing.Size(265, 54);
             this.button3.TabIndex = 4;
             this.button3.Text = "Factura";
             this.button3.UseVisualStyleBackColor = false;
@@ -213,26 +189,19 @@
             this.button4.FlatAppearance.BorderSize = 0;
             this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(237)))), ((int)(((byte)(239)))));
-            this.button4.Location = new System.Drawing.Point(3, 600);
+            this.button4.Location = new System.Drawing.Point(3, 596);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(265, 58);
             this.button4.TabIndex = 5;
             this.button4.Text = "Salir";
             this.button4.UseVisualStyleBackColor = false;
             // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Location = new System.Drawing.Point(9, 6);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(83, 80);
-            this.pictureBox1.TabIndex = 6;
-            this.pictureBox1.TabStop = false;
-            // 
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(73)))), ((int)(((byte)(94)))));
             this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(237)))), ((int)(((byte)(239)))));
-            this.label1.Location = new System.Drawing.Point(108, 6);
+            this.label1.Location = new System.Drawing.Point(117, 7);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(125, 16);
             this.label1.TabIndex = 7;
@@ -242,7 +211,7 @@
             // 
             this.dl_roluser.AutoSize = true;
             this.dl_roluser.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(237)))), ((int)(((byte)(239)))));
-            this.dl_roluser.Location = new System.Drawing.Point(108, 70);
+            this.dl_roluser.Location = new System.Drawing.Point(117, 70);
             this.dl_roluser.Name = "dl_roluser";
             this.dl_roluser.Size = new System.Drawing.Size(48, 16);
             this.dl_roluser.TabIndex = 10;
@@ -252,7 +221,7 @@
             // 
             this.dl_username.AutoSize = true;
             this.dl_username.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(237)))), ((int)(((byte)(239)))));
-            this.dl_username.Location = new System.Drawing.Point(108, 22);
+            this.dl_username.Location = new System.Drawing.Point(117, 22);
             this.dl_username.Name = "dl_username";
             this.dl_username.Size = new System.Drawing.Size(67, 16);
             this.dl_username.TabIndex = 8;
@@ -261,8 +230,9 @@
             // label2
             // 
             this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(73)))), ((int)(((byte)(94)))));
             this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(237)))), ((int)(((byte)(239)))));
-            this.label2.Location = new System.Drawing.Point(108, 54);
+            this.label2.Location = new System.Drawing.Point(117, 54);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(100, 16);
             this.label2.TabIndex = 9;
@@ -271,14 +241,14 @@
             // panel3
             // 
             this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(55)))), ((int)(((byte)(71)))));
-            this.panel3.Controls.Add(this.pictureBox1);
+            this.panel3.Controls.Add(this.pb_typeofuser);
             this.panel3.Controls.Add(this.dl_roluser);
             this.panel3.Controls.Add(this.dl_username);
             this.panel3.Controls.Add(this.label2);
             this.panel3.Controls.Add(this.label1);
-            this.panel3.Location = new System.Drawing.Point(3, 0);
+            this.panel3.Location = new System.Drawing.Point(3, 3);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(259, 94);
+            this.panel3.Size = new System.Drawing.Size(259, 104);
             this.panel3.TabIndex = 11;
             // 
             // panel4
@@ -286,7 +256,7 @@
             this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(55)))), ((int)(((byte)(71)))));
             this.panel4.Controls.Add(this.label3);
             this.panel4.Controls.Add(this.dl_disconnected);
-            this.panel4.Location = new System.Drawing.Point(0, 100);
+            this.panel4.Location = new System.Drawing.Point(0, 113);
             this.panel4.Name = "panel4";
             this.panel4.Size = new System.Drawing.Size(265, 38);
             this.panel4.TabIndex = 12;
@@ -311,11 +281,45 @@
             this.label3.TabIndex = 1;
             this.label3.Text = "Connected";
             // 
+            // pb_typeofuser
+            // 
+            this.pb_typeofuser.Image = global::TCamaleonApp.Properties.Resources.user__questionable_;
+            this.pb_typeofuser.Location = new System.Drawing.Point(9, 7);
+            this.pb_typeofuser.Name = "pb_typeofuser";
+            this.pb_typeofuser.Size = new System.Drawing.Size(89, 79);
+            this.pb_typeofuser.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pb_typeofuser.TabIndex = 6;
+            this.pb_typeofuser.TabStop = false;
+            // 
+            // btn_close
+            // 
+            this.btn_close.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_close.Image = global::TCamaleonApp.Properties.Resources.close;
+            this.btn_close.Location = new System.Drawing.Point(999, 3);
+            this.btn_close.Name = "btn_close";
+            this.btn_close.Size = new System.Drawing.Size(30, 29);
+            this.btn_close.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.btn_close.TabIndex = 0;
+            this.btn_close.TabStop = false;
+            this.btn_close.Click += new System.EventHandler(this.btn_close_Click);
+            // 
+            // btn_minimize
+            // 
+            this.btn_minimize.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_minimize.Image = global::TCamaleonApp.Properties.Resources.minus;
+            this.btn_minimize.Location = new System.Drawing.Point(964, 4);
+            this.btn_minimize.Name = "btn_minimize";
+            this.btn_minimize.Size = new System.Drawing.Size(29, 28);
+            this.btn_minimize.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.btn_minimize.TabIndex = 1;
+            this.btn_minimize.TabStop = false;
+            this.btn_minimize.Click += new System.EventHandler(this.btn_minimize_Click);
+            // 
             // Lobby
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1155, 692);
+            this.ClientSize = new System.Drawing.Size(1032, 688);
             this.Controls.Add(this.ContentPanel);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Lobby";
@@ -324,13 +328,13 @@
             this.ContentPanel.ResumeLayout(false);
             this.panel2.ResumeLayout(false);
             this.panel1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.btn_close)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.btn_minimize)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.panel3.ResumeLayout(false);
             this.panel3.PerformLayout();
             this.panel4.ResumeLayout(false);
             this.panel4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_typeofuser)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btn_close)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btn_minimize)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -351,7 +355,7 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Label dl_roluser;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pb_typeofuser;
         private System.Windows.Forms.Panel panel4;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label dl_disconnected;
