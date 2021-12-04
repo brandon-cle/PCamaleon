1583d107396b3f722a1b7d80755dcd7b8ac44412
brandon-cle <leninbrandon805@gmail.com>
2021-12-03 22:59:55 -0600

    Revert "Merge branch 'EmployeesFromandMain' into otra"
    This reverts commit 848af1ccdfa627f0095dfa5e4923c6d35333a04c, reversing
    changes made to 9e5cdffdbdf8d9258156964d9406ccaa1fd3d188.

--- TCamaleonApp/Model/MEmployeer.cs
+++ TCamaleonApp/Model/MEmployeer.cs
@@ -299,5 +299,37 @@ namespace TCamaleonApp.Model
             }
             return DtResultado;
         }
+
+        public static DataTable DataToShow()
+        {
+            DataTable DtResultado = new DataTable("Givemedata");
+            SqlConnection SqlCon = new SqlConnection();
+            Connection connection = new Connection();
+
+            try
+            {    // Cargando el conexión al servidor
+                SqlCon.ConnectionString = connection.cn;
+                // Creando un objeto SQLCommand que llamará al procedimiento almacenado
+                SqlCommand SqlCmd = new SqlCommand();
+                SqlCmd.Connection = SqlCon;
+                SqlCmd.CommandText = "Givemedata";
+                SqlCmd.CommandType = CommandType.StoredProcedure;
+
+                SqlDataAdapter SqlDat = new SqlDataAdapter(SqlCmd);
+                SqlDat.Fill(DtResultado);
+
+            }
+            catch (Exception ex)
+            {
+                DtResultado = null;
+            }
+            finally
+            {
+                SqlCon.Close();
+            }
+            return DtResultado;
+        }
+
+
     }
 }
