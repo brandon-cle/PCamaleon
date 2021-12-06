57ed5c2b5ef421b99d1bf4eff75b1342b1e37e62
brandon-cle <leninbrandon805@gmail.com>
2021-12-05 19:13:58 -0600

    Some changes wirt report viewer

--- TCamaleonApp/Program.cs
+++ TCamaleonApp/Program.cs
@@ -17,7 +17,7 @@ namespace TCamaleonApp
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form_Login());
+            Application.Run(new Tester());
         }
     }
 }
